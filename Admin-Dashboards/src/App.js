1b330474b7028a933110f91255f5af3d63d56997
BlackDranzer777 <35491850+BlackDranzer777@users.noreply.github.com>
2021-09-05 02:00:41 +0530

    Add files via upload

---
+++ Admin-Dashboards/src/App.js
@@ -0,0 +1,28 @@
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import React from 'react';
+import './App.css';
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+
+function App() {
+  return (
+    <Router>
+      <Topbar/>
+      <div className="container">
+        <Sidebar/>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/users">
+            <UserList/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
